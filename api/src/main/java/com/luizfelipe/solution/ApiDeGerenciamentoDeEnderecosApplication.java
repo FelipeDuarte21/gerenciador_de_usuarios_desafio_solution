86d74ff45c523456c11b88ccfb921b9b4066e348
Luiz Felipe Duarte Elias <felipe15lfde@gmail.com>
2024-11-21 22:01:43 -0300

    Subindo o código fonte base da API do projeto

---
+++ api/src/main/java/com/luizfelipe/solution/ApiDeGerenciamentoDeEnderecosApplication.java
@@ -0,0 +1,13 @@
+package com.luizfelipe.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeGerenciamentoDeEnderecosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeGerenciamentoDeEnderecosApplication.class, args);
+	}
+
+}
