5750e8186778577e72e05d2d2341f6e05593da34
Luiz Felipe Duarte Elias <felipe15lfde@gmail.com>
2024-11-25 00:41:41 -0300

    Refatorando o arquivo api.js

--- app/src/services/api.js
+++ app/src/services/api.js
@@ -4,28 +4,30 @@ const api = axios.create({
     baseURL: 'http://localhost:8080'
 });
 
+const URL_ENDPOINT = "/api/v1/usuario";
+
 export const buscarUsuarios = async () => {
-    const resp = await api.get("/api/v1/usuario");
+    const resp = await api.get(`${URL_ENDPOINT}`);
     return resp.data;
 }
 
 export const cadatrarUsuario = async (dados) => {
-    const resp = await api.post(`/api/v1/usuario`, dados);
+    const resp = await api.post(`${URL_ENDPOINT}`, dados);
     return resp.data;
 }
 
 export const buscarUsuarioPorId = async (id) => {
-    const resp = await api.get(`/api/v1/usuario/${id}`);
+    const resp = await api.get(`${URL_ENDPOINT}/${id}`);
     return resp.data;
 }
 
 export const atualizarUsuario = async (id, dados) => {
-    const resp = await api.put(`/api/v1/usuario/${id}`, dados);
+    const resp = await api.put(`${URL_ENDPOINT}/${id}`, dados);
     return resp.data;
 }
 
 export const excluirUsuario = async (id) => {
-    const resp = await api.delete(`/api/v1/usuario/${id}`);
+    const resp = await api.delete(`${URL_ENDPOINT}/${id}`);
     return resp.data;
 }
 
